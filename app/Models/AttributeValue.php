d1bd47157ee0229acdee702997eb3a2850b529fb
Salam <salamhmb@gmail.com>
2019-10-09 22:05:34 +0100

    Add Attribute and Value Models and Migrations

---
+++ app/Models/AttributeValue.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class AttributeValue
+ * @package App\Models
+ */
+class AttributeValue extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'attribute_values';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'attribute_id', 'value', 'price'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'attribute_id'  =>  'integer',
+    ];
+
+    public function attribute()
+    {
+        return $this->belongsTo(Attribute::class);
+    }
+}
